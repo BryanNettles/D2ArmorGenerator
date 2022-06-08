976449ec3fac3f8a667680a5143118c26e7a47c5
William Lee <wkl@explainedbycats.com>
2022-06-08 17:29:21 -0400

    Clean up and instructional changes
    This commit makes a number of changes to clean up some stuff like...
    
    * syntax - uses lowercasing on PS keywords
    * removing some of the inner function calls and streamlining how the switch/string generation is done
    * clarifying some variable names

--- Diablo 2 Armor Generator.ps1
+++ Diablo 2 Armor Generator.ps1
@@ -1,99 +1,70 @@
-﻿$SuffixMods = import-csv -Path ".\SuffixMods.csv"
+﻿$SuffixMods = import-csv -Path (Join-Path -Path $PSScriptRoot -ChildPath "SuffixMods.csv")
 $ArmorTypeArray = @('Normal','Elite','Exceptional','Baller')
 
-Function Get-RElementalModifier($C)
-{
+function Get-RElementalModifiers($cycles) {
+    $range = [System.Collections.Generic.List[int]](0..3)
+    $modifiers = @()
 
-$EStart = new-object system.collections.generic.list[object]
-$Estart.AddRange(0..3)
-$Emod = @()
-$Etrack = @()
-
- Function Get-Modifier ($GMN, $C){
-        
-        Switch ($C)
-        {
-        0 {$GMMin = 5;$GMMax = 11}
-        1 {$GMMin = 11;$GMMax = 26}
-        2 {$GMMin = 26;$GMMax = 36}
-        3 {$GMMin = 36;$GMMax = 51}
-        }
-
-        Switch ($GMN)
-        {
-        0 {$GM = '+' + [string]$(Get-Random -Minimum $GMMin -Maximum $GMMax) + ' Fire resist'}
-        1 {$GM = '+' + [string]$(Get-Random -Minimum $GMMin -Maximum $GMMax) + ' Cold resist'}
-        2 {$GM = '+' + [string]$(Get-Random -Minimum $GMMin -Maximum $GMMax) + ' Lightning resist'}
-        3 {$GM = '+' + [string]$(Get-Random -Minimum $GMMin -Maximum $GMMax) + ' Poison resist'}
-        }
-        Return $GM
+    foreach ($i in 0..$cycles) {
+        $n = Get-Random -InputObject $range
+        $modifiers += $n
+        $range.Remove($n) | out-null
     }
 
-
-$Count = 0
-
-
-
-    While ($Count -le $C){
-
-
-        $Count ++
-        $E = get-random -InputObject $Estart
-        $EMod += $(Get-Modifier -GMN $E)
-        $Estart.Remove($E) | out-null
-
+    $fn = { param ( $rst, $counter)
+        switch ($counter) {
+            0 { $statMin = 5; $statMax = 11 }
+            1 { $statMin = 11; $statMax = 26 }
+            2 { $statMin = 26; $statMax = 36 }
+            3 { $statMin = 36; $statMax = 51 }
+        }
+        return "+{0} $rst resist" -f (Get-Random -Minimum $statMin -Maximum $statMax)
     }
 
-Return $Emod
-
+    $counter = 0
+    $mods = switch ( $modifiers ) {
+        0 { $fn.invoke("Fire", $counter++) }
+        1 { $fn.invoke("Cold", $counter++) }
+        2 { $fn.invoke("Lightning", $counter++) }
+        3 { $fn.invoke("Poison", $counter++) }
+    }
+    return $mods -Join ", "
 }
 
-
-
-
-Function Create-Armor ($C1, $C2, $C3){
-
-    If ($C3 -notin ($ArmorTypeArray)){
-        return "Invalid Armor Tier. Please try again."
+Function Create-Armor ($C1, $C2, $C3) {
+    if ($C3 -notin ($ArmorTypeArray)) {
+        Write-Error "Invalid Armor Tier. Please try again."
+        exit 1
     }
 
+    $Suffix = $SuffixMods[$(get-random -Minimum 0 -Maximum 4)]
+    Write-Host $Suffix
 
-$Suffix = $Suffixmods[$(get-random -Minimum 0 -Maximum 4)]
-
-
-Switch ($C3)
-{
- 'Normal' {$De = Get-Random -minimum 10 -maximum 25;$Dem = .1;$Prefix = 'Bronze';$Emc = 0}
- 'Elite' {$De = Get-Random -minimum 28 -maximum 50;$Dem = .5;$Prefix = 'Silver';$Emc = 1}
- 'Exceptional' {$De = Get-Random -minimum 76 -maximum 100;$Dem = .75;$Prefix = 'Gold';$Emc = 2}
- 'Baller' {$De = Get-Random -minimum 176 -Maximum 200;$Dem = 1.0;$Prefix = "Platnium";$Emc = 3}
-}
-
-$Emod = Get-RElementalModifier -c $Emc
-
-
-
-$A = new-object system.collections.generic.list[pscustomobject]
-
-$A.add([pscustomobject]@{
-    Class1 = [string]$C1
-    Class2 = [string]$C2
-    Class3 = [string]$C3
-    Name = "$Prefix Breast plate $($Suffix.name)"
-    DefenseBase = [int]$De
-    DefenseModifier = [float]$Dem
-    DefenseTotal = [int]($De + ($De * $Dem))
-    ElementalModifier = $Emod
-    SuffixModifier = $Suffix.Modifier
+    switch ($C3) {
+         'Normal' {$De = Get-Random -minimum 10 -maximum 25; $Dem = .1; $Prefix = 'Bronze'; $Emc = 0}
+         'Elite' {$De = Get-Random -minimum 28 -maximum 50; $Dem = .5; $Prefix = 'Silver'; $Emc = 1}
+         'Exceptional' {$De = Get-Random -minimum 76 -maximum 100; $Dem = .75; $Prefix = 'Gold'; $Emc = 2}
+         'Baller' {$De = Get-Random -minimum 176 -Maximum 200; $Dem = 1.0; $Prefix = "Platnium"; $Emc = 3}
     }
-)
 
+    $modifiers = Get-RElementalModifiers -c $Emc
 
-return $A
+    $armor = New-Object System.Collections.Generic.List[pscustomobject]
 
+    $armor.add([pscustomobject]@{
+        Class1 = [string]$C1
+        Class2 = [string]$C2
+        Class3 = [string]$C3
+        Name = "$Prefix Breast plate $($Suffix['Name'])"
+        DefenseBase = [int]$De
+        DefenseModifier = [float]$Dem
+        DefenseTotal = [int]($De + ($De * $Dem))
+        ElementalModifier = $modifiers
+        SuffixModifier = $Suffix.Modifier
+        }
+    )
+    return $armor
 }
 
 
 $Armor = Create-Armor -C1 'Armor' -C2 'Chest' -C3 "$(read-host -Prompt "Select one from $($ArmorTypeArray)")"; $Armor
-
-
