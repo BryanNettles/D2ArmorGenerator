3d291ec7eefa50ec0e41617ff5ef35a0c31ca784
BryanNettles <107131682+BryanNettles@users.noreply.github.com>
2022-06-08 15:42:25 -0500

    Update Diablo 2 Armor Generator.ps1
    Identifiers start with 1 instead of 0. This includes Tier nums, $EStart range, switch statements, etc.

--- Diablo 2 Armor Generator.ps1
+++ Diablo 2 Armor Generator.ps1
@@ -1,46 +1,47 @@
-﻿$SuffixMods = import-csv -Path ".\SuffixMods.csv"
+$SuffixMods = import-csv -Path "C:\Users\brynet01\OneDrive - Robert Half\Powershell Shenanigans\SuffixMods.txt"
 $ArmorTypeArray = @('Normal','Elite','Exceptional','Baller')
 
 Function Get-RElementalModifier($C)
 {
 
+
 $EStart = new-object system.collections.generic.list[object]
-$Estart.AddRange(0..3)
+$Estart.AddRange(1..4)
 $Emod = @()
 $Etrack = @()
 
- Function Get-Modifier ($GMN, $C){
+    Function Get-Modifier ($GMN, $C){
         
         Switch ($C)
         {
-        0 {$GMMin = 5;$GMMax = 11}
-        1 {$GMMin = 11;$GMMax = 26}
-        2 {$GMMin = 26;$GMMax = 36}
-        3 {$GMMin = 36;$GMMax = 51}
+        1 {$GMMin = 5;$GMMax = 11}
+        2 {$GMMin = 11;$GMMax = 26}
+        3 {$GMMin = 26;$GMMax = 36}
+        4 {$GMMin = 36;$GMMax = 51}
         }
 
         Switch ($GMN)
         {
-        0 {$GM = '+' + [string]$(Get-Random -Minimum $GMMin -Maximum $GMMax) + ' Fire resist'}
-        1 {$GM = '+' + [string]$(Get-Random -Minimum $GMMin -Maximum $GMMax) + ' Cold resist'}
-        2 {$GM = '+' + [string]$(Get-Random -Minimum $GMMin -Maximum $GMMax) + ' Lightning resist'}
-        3 {$GM = '+' + [string]$(Get-Random -Minimum $GMMin -Maximum $GMMax) + ' Poison resist'}
+        1 {$GM = '+' + [string]$(Get-Random -Minimum $GMMin -Maximum $GMMax) + ' Fire resist'}
+        2 {$GM = '+' + [string]$(Get-Random -Minimum $GMMin -Maximum $GMMax) + ' Cold resist'}
+        3 {$GM = '+' + [string]$(Get-Random -Minimum $GMMin -Maximum $GMMax) + ' Lightning resist'}
+        4 {$GM = '+' + [string]$(Get-Random -Minimum $GMMin -Maximum $GMMax) + ' Poison resist'}
         }
         Return $GM
     }
 
 
-$Count = 0
+$Count = 1
 
 
 
     While ($Count -le $C){
 
 
-        $Count ++
         $E = get-random -InputObject $Estart
-        $EMod += $(Get-Modifier -GMN $E)
+        $EMod += $(Get-Modifier -GMN $E -C $C)
         $Estart.Remove($E) | out-null
+        $Count ++
 
     }
 
@@ -57,16 +58,15 @@ Function Create-Armor ($C1, $C2, $C3){
         return "Invalid Armor Tier. Please try again."
     }
 
-
 $Suffix = $Suffixmods[$(get-random -Minimum 0 -Maximum 4)]
 
 
 Switch ($C3)
 {
- 'Normal' {$De = Get-Random -minimum 10 -maximum 25;$Dem = .1;$Prefix = 'Bronze';$Emc = 0}
- 'Elite' {$De = Get-Random -minimum 28 -maximum 50;$Dem = .5;$Prefix = 'Silver';$Emc = 1}
- 'Exceptional' {$De = Get-Random -minimum 76 -maximum 100;$Dem = .75;$Prefix = 'Gold';$Emc = 2}
- 'Baller' {$De = Get-Random -minimum 176 -Maximum 200;$Dem = 1.0;$Prefix = "Platnium";$Emc = 3}
+ 'Normal' {$De = Get-Random -minimum 10 -maximum 25;$Dem = .1;$Prefix = 'Bronze';$Emc = 1}
+ 'Elite' {$De = Get-Random -minimum 28 -maximum 50;$Dem = .5;$Prefix = 'Silver';$Emc = 2}
+ 'Exceptional' {$De = Get-Random -minimum 76 -maximum 100;$Dem = .75;$Prefix = 'Gold';$Emc = 3}
+ 'Baller' {$De = Get-Random -minimum 176 -Maximum 200;$Dem = 1.0;$Prefix = "Platnium";$Emc = 4}
 }
 
 $Emod = Get-RElementalModifier -c $Emc
@@ -95,5 +95,4 @@ return $A
 
 
 $Armor = Create-Armor -C1 'Armor' -C2 'Chest' -C3 "$(read-host -Prompt "Select one from $($ArmorTypeArray)")"; $Armor
-
 
